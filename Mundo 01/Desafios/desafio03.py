0a072c49af70bb93ac2c91bac61a2a99d500982e
Marco Targino <maa.targino@gmail.com>
2021-05-06 19:19:19 -0300

    changes in sum calculator

--- Mundo 01/Desafios/desafio03.py
+++ Mundo 01/Desafios/desafio03.py
@@ -1,5 +1,5 @@
-print('Enter with numbers')
+print('SUM CALCULATOR')
 n1 = int(input('Enter with the first number: ')) 
 n2 = int(input('Enter with the second number: ')) 
 sum = int(n1+n2)
-print('The sum result is {}'.format(sum))
+print('The sum result of {} plus {} is {}'.format(n1,n2,sum))
