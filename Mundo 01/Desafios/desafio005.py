f3049524c3fe1d11cf0bc09e8f9d72579430e37d
Marco Targino <maa.targino@gmail.com>
2021-05-07 11:49:25 -0300

    desafios resolvidos do 5 ao 8

---
+++ Mundo 01/Desafios/desafio005.py
@@ -0,0 +1,3 @@
+print('CALCULADORA DE SUCESSOR E ANTECESSOR')
+n = int(input('Digite um número qualquer: '))
+print('O seu sucessor é {} e o antecessor é {}.'.format(n+1,n-1))
---
+++ Mundo 01/Desafios/desafio006.py
@@ -0,0 +1,3 @@
+print('CALCULADORA DE MÚLTIPLOS E RAIZ QUADRADA')
+n = int(input('Entre com um número qualquer: '))
+print('O seu dobro é: {}\nO seu triplo é: {}\nE a sua raiz quadrada é: {}'.format(n*2,n*3,n**(1/2)))
