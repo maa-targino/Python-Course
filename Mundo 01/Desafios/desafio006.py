d9cad69b4cd65d914aaba3d715effc1690661a10
Marco Targino <maa.targino@gmail.com>
2021-05-07 15:49:02 -0300

    import sqrt

--- Mundo 01/Desafios/desafio006.py
+++ Mundo 01/Desafios/desafio006.py
@@ -1,3 +1,4 @@
+from math import sqrt
 print('CALCULADORA DE MÚLTIPLOS E RAIZ QUADRADA')
 n = int(input('Entre com um número qualquer: '))
-print('O seu dobro é: {}\nO seu triplo é: {}\nE a sua raiz quadrada é: {}'.format(n*2,n*3,n**(1/2)))
+print('O seu dobro é: {}\nO seu triplo é: {}\nE a sua raiz quadrada é: {}'.format(n*2,n*3,sqrt(n)))
