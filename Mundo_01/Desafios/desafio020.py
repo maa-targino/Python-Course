f70daa234892f20b4f529cc642789a0b7d8b7fa3
Marco Targino <maa.targino@gmail.com>
2021-05-11 13:22:31 -0300

    add desafio020

--- Mundo_01/Desafios/desafio020.py
+++ Mundo_01/Desafios/desafio020.py
@@ -1,11 +1,11 @@
-from random import choice
+from random import shuffle
 
-print('SORTEIO DE ALUNO')
 aluno1 = input('Entre com o primeiro nome: ')
 aluno2 = input('Entre com o segundo nome: ')
 aluno3 = input('Entre com o terceiro nome: ')
 aluno4 = input('Entre com o quarto nome: ')
 
 lista = [aluno1, aluno2, aluno3, aluno4]
+shuffle(lista)
 
-print('O aluno escolhido foi: {}'.format(choice(lista)))
+print('Os trabalhos vão ser apresentados respectivamente pelos alunos: {}'.format(lista))
